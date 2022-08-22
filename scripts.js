eddce788d284b708cf33775decb67453bf5ebec3
Marlon Junior <marlonrfjunior@outlook.com>
2022-08-22 16:04:13 -0300

    Adicionando Level game

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
-const cards = document.querySelectorAll('.memory-card');
+var cards = document.querySelectorAll('.memory-card');
+
+var cardsToLoad = [{name: "angular"}
+                   ,{name: "aurelia"}
+                   ,{name: "backbone"}
+                   ,{name: "ember"}
+                   ,{name: "react"}
+                   ,{name: "vue"}];
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
@@ -48,11 +55,43 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
-(function shuffle() {
+
+
+
+
+function shuffle() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
-})();
+}
 
+function loadCards(level) {
+
+  let indexLevel = 0
+  let card_section = document.querySelector(".memory-game");
+  card_section.innerHTML = ""
+  cardsToLoad.forEach((el) => {
+    if (indexLevel <= level) {
+        indexLevel++
+        let identificador = el.name;
+        let card_container = `
+
+              <div class="memory-card" data-framework="${identificador}">
+                <img class="front-face" src="img/${identificador}.svg" alt="Face da Carta">
+                <img class="back-face" src="img/js-badge.svg" alt="Verso da Carta">
+              </div>
+              <div class="memory-card" data-framework="${identificador}">
+                <img class="front-face" src="img/${identificador}.svg" alt="Face da Carta">
+                <img class="back-face" src="img/js-badge.svg" alt="Verso da Carta">
+              </div>
+`;
+      card_section.innerHTML += card_container;
+  }});
+    cards = document.querySelectorAll('.memory-card');
+      shuffle()
+    cards.forEach(card => card.addEventListener('click', flipCard));
+}
+
+//document.querySelector("#btnEasy").addEventListener("click", loadCards());
 cards.forEach(card => card.addEventListener('click', flipCard));
